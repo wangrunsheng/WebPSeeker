4190d3e40c2e94fe252cf3d143f8a5f52a9e7e78
wangrunsheng <abc199035>
2020-12-31 15:34:42 +0800

    read douban permission failed.

--- app/src/main/java/wang/runsheng/webpseeker/MainActivity.java
+++ app/src/main/java/wang/runsheng/webpseeker/MainActivity.java
@@ -3,12 +3,30 @@ package wang.runsheng.webpseeker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private String path = "/Android/data/com.douban.frodo/cache/images/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String sdcard = Environment.getExternalStorageDirectory().getPath();
+        Log.d(TAG, "path: " + sdcard + path);
+        File dir = new File(sdcard + path);
+        dir = new File("content://com.android.fileexplorer.myprovider/external_files" + path);
+        if (dir.isDirectory()) {
+            File[] images = dir.listFiles();
+            if (images != null) {
+                Log.d(TAG, "douban has: " + images.length);
+            }
+        }
     }
 }
