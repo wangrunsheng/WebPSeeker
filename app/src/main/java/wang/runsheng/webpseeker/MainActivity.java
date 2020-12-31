5ca561b64fb73e2a0b5f542549aad7f16a5d6c73
wangrunsheng <abc199035>
2020-12-31 13:04:56 +0800

    Initial commit

---
+++ app/src/main/java/wang/runsheng/webpseeker/MainActivity.java
@@ -0,0 +1,14 @@
+package wang.runsheng.webpseeker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
